09206b66015ae1af696986bde61163d99684bf83
Yong Jian Ming <jianming1993@gmail.com>
2025-05-04 23:35:41 +0800

    Update new pages

---
+++ ViewModels/CharacterDetail/CharacterDetailViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+using SlimeIMWiki.Models;
+using SlimeIMWiki.Services;
+
+namespace SlimeIMWiki.ViewModels.CharacterDetail;
+
+public sealed partial class CharacterDetailViewModel : ReactiveObject, IActivatableViewModel
+{
+    public ViewModelActivator Activator { get; } = new();
+
+    [ObservableAsProperty]
+    private ICharacterUnit? _unit;
+    
+    private readonly JsonDataModelService _jsonDataModelService;
+
+    public CharacterDetailViewModel(string permalink, JsonDataModelService jsonDataModelService)
+    {
+        _jsonDataModelService = jsonDataModelService;
+        
+        _unitHelper = this.WhenAnyValue(
+                model => model._jsonDataModelService.BattleUnits,
+                model => model._jsonDataModelService.ProtectionUnits,
+                (battleUnits, protectionUnits) => battleUnits.Cast<ICharacterUnit>().Concat(protectionUnits))
+            .SelectMany(units => units)
+            .FirstOrDefaultAsync(unit => unit.Permalink.Equals(permalink, StringComparison.InvariantCultureIgnoreCase))
+            .ToProperty(this, nameof(Unit));
+    }
+
+    public string? GetTacticTypeImage()
+    {
+        return Unit is null ? null : _jsonDataModelService.GetTacticType(Unit.TacticsType)?.Image;
+    }
+    
+    public string? GetForceIcon(string force)
+    {
+        return Unit is null ? null : _jsonDataModelService.GetForce(force)?.Icon;
+    }
+}
