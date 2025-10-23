e7d1b09b3dab3748f6a81ffb92e937df968f54b9
Yong Jian Ming <jianming1993@gmail.com>
2025-10-23 22:32:55 +0800

    Test service worker fix [skip ci]

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -8,8 +8,8 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.webp$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.webmanifest$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/, /^\.gitignore/, /^image/ ];
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.webp$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.webmanifest$/];
+const offlineAssetsExclude = [/^service-worker\.js$/, /^\.gitignore/, /^image/];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
@@ -23,7 +23,8 @@ async function onInstall(event) {
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+        .map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
+
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
@@ -32,6 +33,7 @@ async function onActivate(event) {
 
     // Delete unused caches
     const cacheKeys = await caches.keys();
+
     await Promise.all(cacheKeys
         .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
         .map(key => caches.delete(key)));
@@ -39,6 +41,7 @@ async function onActivate(event) {
 
 async function onFetch(event) {
     let cachedResponse = null;
+
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache,
         // unless that request is for an offline resource.
@@ -47,14 +50,17 @@ async function onFetch(event) {
             && !manifestUrlList.some(url => url === event.request.url);
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
+
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
-        
+
         if (!cachedResponse) {
             const missingAssets = self.assetsManifest.assets
-                .filter(asset => asset.url === event.request.url)
-                .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+                .filter(asset => new URL(asset.url, baseUrl).href === event.request.url)
+                .map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
+
             await cache.addAll(missingAssets);
+
             cachedResponse = await cache.match(request);
         }
     }
