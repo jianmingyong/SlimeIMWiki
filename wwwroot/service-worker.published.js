4a3ca782cd3518f215182461944e54550f5f12a8
Yong Jian Ming <jianming1993@gmail.com>
2026-02-12 22:54:05 +0800

    Fix some stuff

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -43,30 +43,26 @@ async function onActivate(event) {
 }
 
 async function onFetch(event) {
-    let cachedResponse = null;
+    let request = event.request;
 
     if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache,
-        // unless that request is for an offline resource.
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
             && !manifestUrlList.some(url => url === event.request.url);
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-
-        if (!cachedResponse) {
-            const missingAssets = self.assetsManifest.assets
-                .filter(asset => new URL(asset.url, baseUrl).href === event.request.url)
-                .map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
-
-            await cache.addAll(missingAssets);
-
-            cachedResponse = await cache.match(request);
-        }
+        request = shouldServeIndexHtml ? 'index.html' : event.request;
     }
 
-    return cachedResponse || fetch(event.request);
+    try {
+        const networkResponse = await fetch(request);
+
+        if (networkResponse.ok) {
+            const cache = await caches.open(cacheName);
+            await cache.put(request, networkResponse.clone())
+        }
+        
+        return networkResponse;
+    } catch (error) {
+        const cachedResponse = await caches.match(request);
+        return cachedResponse || Response.error();
+    }
 }
