4f65b4412ef676388eae7e53137e9546fda4427c
Yong Jian Ming <jianming1993@gmail.com>
2026-02-13 18:50:22 +0800

    Fix cache issues and make the data fetch from origin if possible.

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -12,7 +12,9 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.webp$/, /\.ico$/, /\.blat$/, /\.dat$/, /\.webmanifest$/];
-const offlineAssetsExclude = [/^service-worker\.js$/, /^service-worker-assets\.js$/, /^\.gitignore/, /^image/];
+const offlineAssetsExclude = [/^service-worker\.js$/, /^service-worker-assets\.js$/, /^\.gitignore$/, /^image\//];
+const offlineAssetsIntegrityExclude = [/\.html$/];
+const offlineAssetsPreferLoadFromOrigin = [/^data\//];
 
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
@@ -26,7 +28,15 @@ async function onInstall(event) {
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
+        .map(asset => {
+            const skipIntegrityCheck = offlineAssetsIntegrityExclude.some(pattern => pattern.test(asset.url));
+
+            if (skipIntegrityCheck) {
+                return new Request(asset.url, { cache: 'no-cache' });
+            } else {
+                return new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' })
+            }
+        });
 
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
@@ -43,30 +53,55 @@ async function onActivate(event) {
 }
 
 async function onFetch(event) {
-    let cachedResponse = null;
+    let request = event.request;
+    let response;
 
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache,
         // unless that request is for an offline resource.
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !manifestUrlList.some(url => url === event.request.url);
+        const shouldServeIndexHtml = event.request.mode === 'navigate' && !manifestUrlList.some(url => url === event.request.url);
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-
-        if (!cachedResponse) {
-            const missingAssets = self.assetsManifest.assets
-                .filter(asset => new URL(asset.url, baseUrl).href === event.request.url)
-                .map(asset => new Request(asset.url, {integrity: asset.hash, cache: 'no-cache'}));
-
-            await cache.addAll(missingAssets);
-
-            cachedResponse = await cache.match(request);
+        if (shouldServeIndexHtml) {
+            request = new Request(new URL('index.html', baseUrl));
         }
     }
 
-    return cachedResponse || fetch(event.request);
+    const preferLoadFromOrigin = offlineAssetsPreferLoadFromOrigin.some(pattern => pattern.test(new URL(request.url).pathname.slice(1)));
+
+    if (preferLoadFromOrigin) {
+        response = await fetch(request, { cache: 'no-cache' });
+
+        const cache = await caches.open(cacheName);
+
+        if (!response.ok) {
+            response = await cache.match(request);
+        } else {
+            await cache.put(request, response.clone());
+        }
+    } else {
+        const cache = await caches.open(cacheName);
+        response = await cache.match(request);
+
+        if (!response) {
+            const skipIntegrityCheck = offlineAssetsIntegrityExclude.some(pattern => pattern.test(new URL(request.url).pathname.slice(1)));
+
+            if (skipIntegrityCheck) {
+                response = await fetch(request, { cache: 'no-cache' });
+                await cache.put(request, response.clone());
+            } else {
+                const asset = self.assetsManifest.assets.find(asset => new URL(asset.url, baseUrl).href === request.url);
+
+                if (asset) {
+                    response = await fetch(request, { integrity: asset.hash, cache: 'no-cache' });
+                } else {
+                    response = await fetch(request, { cache: 'no-cache' });
+                }
+
+                await cache.put(request, response.clone());
+            }
+        }
+    }
+
+    return response ?? Response.error();
 }
